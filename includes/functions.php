d1c59b1dea485f27b81a994616883310d3917030
Varun Sridharan <varunsridharan23@gmail.com>
2021-03-16 14:02:14 +0530

    Updated

--- includes/functions.php
+++ includes/functions.php
@@ -10,3 +10,31 @@ if ( ! function_exists( 'wp_sandbox_helper_get_current_demo_item' ) ) {
 		return ( WP_SANDBOX_LOCAL_DOMAIN === $site->domain ) ? WP_SANDBOX_LOCAL_DOMAIN_SITE : str_replace( WP_SANDBOX_MAIN_DOMAIN, '', $site->domain );
 	}
 }
+
+if ( ! function_exists( 'wp_sandbox_helper_other_logins' ) ) {
+	/**
+	 * Returns A List of other logins.
+	 *
+	 * @return \string[][]
+	 */
+	function wp_sandbox_helper_other_logins() {
+		return array(
+			'Customer'     => array(
+				'customer1' => 'demo',
+				'customer2' => 'demo',
+			),
+			'Pro Customer' => array(
+				'procustomer1' => 'demo',
+				'procustomer2' => 'demo',
+			),
+			'Wholesaler'   => array(
+				'seller1' => 'demo',
+				'seller2' => 'demo',
+			),
+			'Shop manager' => array(
+				'manager1' => 'demo',
+				'manager2' => 'demo',
+			),
+		);
+	}
+}
--- includes/wp-shortcode.php
+++ includes/wp-shortcode.php
@@ -25,6 +25,19 @@ final class Shortcode {
 		if ( isset( $item['name'] ) ) {
 			$html .= '<h3>Launch your ' . $item['name'] . ' Demo Today</h3>';
 		}
+
+		$html .= do_shortcode( '[try_demo title="To create your demo website provide the following data" label="Your email:" placeholder="example@mail.com" submit_btn="Submit" success="An activation email was sent to your email address." fail="An error has occurred. Please notify the website Administrator." captcha="1" source_id="1"]An activation email will be sent to this email address. After the confirmation you will be redirected to WordPress Dashboard.[/try_demo]' );
+
+		$html .= '<h3>Other Login information’s</h3>
+<p>you can also login as other user to test our plugins</p>';
+
+		foreach ( wp_sandbox_helper_other_logins() as $role => $login ) {
+			$html .= "<h4 style='margin-top:0;'>${role}</h4> <ul>";
+			foreach ( $login as $username => $password ) {
+				$html .= "<li> ${username} / ${password} </li>";
+			}
+			$html .= '</ul>';
+		}
 		return $html;
 	}
 }
